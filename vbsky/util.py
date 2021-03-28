479811916df22a95fbcd078a2c4ca8b72aa440f8
Jonathan Terhorst <terhorst@gmail.com>
2021-03-28 12:16:49 -0400

    remove dead util code

--- vbsky/util.py
+++ vbsky/util.py
@@ -1,16 +1,8 @@
-import csv
 import functools
-from typing import Dict, NamedTuple, Tuple
+from typing import NamedTuple
 
-import jax
 import numpy as np
-import tskit
-from Bio import SeqIO
 from jax import numpy as jnp
-from jax.experimental.host_callback import id_tap
-from jax.lib import pytree
-
-from .tree_data import TreeData
 
 
 class TipData(NamedTuple):
@@ -24,62 +16,6 @@ def jaxify_args(f):
         return f(*map(jnp.array, args))
 
     return _f
-
-
-def get_dates(dates_file):
-    reader = csv.reader(open(dates_file, "r"))
-    node_dates_dict = {row[0]: float(row[1]) for row in reader}
-    return node_dates_dict
-
-
-DNA_MAP = dict(zip("acgt", np.eye(4)))
-
-
-def get_partials(alignment: Dict[int, SeqIO.SeqRecord]) -> np.ndarray:
-    """Convert an alignment to array of tip partial likelihoods."""
-    num_samples = len(alignment)
-    assert num_samples > 0
-    num_sites = len(alignment[0].seq)
-    partials = np.zeros((num_sites, num_samples, 4), dtype=int)
-
-    for s in range(num_samples):
-        for i, c in enumerate(alignment[s].seq):
-            # all missing values are encoded with [1,1,1,1]
-            partials[i, s] = DNA_MAP.get(c.lower(), [1, 1, 1, 1])
-    return partials
-
-
-def parse_tsk_tree(tree: tskit.Tree) -> Tuple[TreeData, jnp.ndarray]:
-    G = tree.genotype_matrix()  # [M, n]
-
-
-def scan_with_init(f, init, xs, length=None, reverse=False):
-    """Wrapper around jax.lax.scan that adds the initial value(s) onto the returned sequence.
-
-    Examples:
-        # >>> scan_with_init(lambda carry, x: (x, x), -1, jnp.arange(3))  # doctest: +NORMALIZE_WHITESPACE
-        # (DeviceArray(2, dtype=int32), DeviceArray([-1, 0, 1, 2], dtype=int32))
-        # >>> # compute k-chooses-2
-        # >>> scan_with_init(lambda carry, x: (carry + x, carry + x), 1, jnp.arange(2, 10))[1]  # doctest: +NORMALIZE_WHITESPACE
-        # DeviceArray([ 1, 3, 6, 10, 15, 21, 28, 36, 45], dtype=int32)
-        # >>> # 10 + 9, 10 + 9 + 8, ...
-        >>> scan_with_init(lambda carry, x: (carry + x, carry + x), 10, jnp.arange(1, 10), reverse=True)[1]  # doctest: +NORMALIZE_WHITESPACE
-        DeviceArray([55, 54, 52, 49, 45, 40, 34, 27, 19, 10], dtype=int32)
-        >>> scan_with_init(lambda carry, x: (x, x), {'a': -1}, {'a': jnp.arange(3)})  # doctest: +NORMALIZE_WHITESPACE
-        ({'a': DeviceArray(2, dtype=int32)}, {'a': DeviceArray([-1, 0, 1, 2], dtype=int32)})
-    """
-    carry, ys = jax.lax.scan(f, init, xs, length, reverse)
-    if reverse:
-        ys, init = init, ys
-    ys = jax.tree_util.tree_multimap(jnp.append, init, ys)
-    return carry, ys
-
-
-def id_assert(x, cond, msg):
-    def _f(x):
-        assert cond(x), msg
-
-    return id_tap(_f, x)
 
 
 @jaxify_args
@@ -129,25 +65,6 @@ def order_events(times, node_heights, node_is_sample):
     j = merged.argsort()
     delta = jnp.concatenate([jnp.zeros_like(times), 2 * node_is_sample - 1])
     return jnp.array([merged[j], jnp.cumsum(delta[j])]).T
-
-
-def tree_stack(trees):
-    """Takes a list of trees and stacks every corresponding leaf.
-    For example, given two trees ((a, b), c) and ((a', b'), c'), returns
-    ((stack(a, a'), stack(b, b')), stack(c, c')).
-    Useful for turning a list of objects into something you can feed to a
-    vmapped function.
-    """
-    leaves_list = []
-    treedef_list = []
-    for tree in trees:
-        leaves, treedef = pytree.flatten(tree)
-        leaves_list.append(leaves)
-        treedef_list.append(treedef)
-
-    grouped_leaves = zip(*leaves_list)
-    result_leaves = [jnp.stack(l) for l in grouped_leaves]
-    return treedef_list[0].unflatten(result_leaves)
 
 
 class RateFunction(NamedTuple):
